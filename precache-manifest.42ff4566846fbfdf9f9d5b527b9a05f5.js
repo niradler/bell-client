d076a9f7f670d43dc2c9c1cf9484533825783d95
Nir Adler <niradler@users.noreply.github.com>
2020-08-15 19:52:23 +0300

    Updates

---
+++ precache-manifest.42ff4566846fbfdf9f9d5b527b9a05f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a703be145a924473a271400a1a7c0e9",
+    "url": "/bell-client/index.html"
+  },
+  {
+    "revision": "b98cefce17051947a0a3",
+    "url": "/bell-client/static/css/main.e16a88bc.chunk.css"
+  },
+  {
+    "revision": "06a3ad4ebaf14b1c2b74",
+    "url": "/bell-client/static/js/2.e3b2587e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bell-client/static/js/2.e3b2587e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98cefce17051947a0a3",
+    "url": "/bell-client/static/js/main.52a9ec69.chunk.js"
+  },
+  {
+    "revision": "46a055212b0f4518ea16",
+    "url": "/bell-client/static/js/runtime-main.7da48375.js"
+  }
+]);
